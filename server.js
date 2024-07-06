bcacebb11d9d10a4e24c8e74cc7a04bfd3900df9
xsm3z <xsm.zhang@gmail.com>
2024-07-06 12:33:26 -0400

    1-2 complete

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const express = require('express')
+const express = require('express');
+const logger = require('morgan');
 
-const app = express()
+
+const app = express();
+app.use(logger('dev'));
+
+app.get('/greetings/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(`Hello there, ${username}.`)
+})
+
+app.get('/roll/:number', (req, res) => {
+  const number = (req.params.number);
+  const roll = Math.floor(Math.random());
+  if (isNaN(number)) {
+    res.send('You must specify a number');
+  } else {
+    res.send(`You rolled a ${roll}.`)
+  }
+})
 
 app.listen(3000, () => {
   console.log('I am listening')
